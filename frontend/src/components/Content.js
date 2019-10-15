157d24573d814b61af4049f4f032e8d49a4bb6e1
Syd <green@Syds-Air-2.lan1>
2019-10-15 18:27:47 -0500

    Done.

--- frontend/src/components/Content.js
+++ frontend/src/components/Content.js
@@ -10,18 +10,26 @@ import Instructions from './Instructions';
           Then complete the rest of your app before attempting to
           refactor to get this Content component to work.
 */
+
+
+
 class Content extends Component {
-  renderContent = () => {
-    if (false) {
-      return <NoteEditor />;
-    } else if (false) {
-      return <NoteViewer />;
+  renderContent = (props) => {
+    if (this.props.editStatus === true) {
+      return <NoteEditor currentSelection={this.props.currentSelection} handleSave={this.props.handleSave} editedSelection={this.props.editedSelection}/>;
+    } else if (this.props.editedSelection) {
+      return <NoteViewer currentSelection={this.props.currentSelection} handleEdit={this.props.handleEdit} editedSelection={this.props.editedSelection}/>;
+    } else if (this.props.currentSelection !== 0) {
+      return <NoteViewer currentSelection={this.props.currentSelection} handleEdit={this.props.handleEdit} editedSelection={this.props.editedSelection}/>;
     } else {
       return <Instructions />;
     }
   }
 
+
+
   render() {
+    console.log(this.props)
     return (
       <div className='master-detail-element detail'>
         {this.renderContent()}
--- frontend/src/components/NoteViewer.js
+++ frontend/src/components/NoteViewer.js
@@ -1,13 +1,24 @@
 import React, { Fragment } from 'react';
 
 const NoteViewer = (props) => {
-  return (
+  if (props.editedSelection) {
+    return (
     <Fragment>
-      <h2>Title</h2>
-      <p>Body</p>
-      <button>Edit</button>
-    </Fragment>
-  );
+      <h2>{props.editedSelection.title}</h2>
+      <p>{props.editedSelection.body}</p>
+      <button onClick={props.handleEdit}>Edit</button>
+    </Fragment> )}
+    else {
+      return (
+        <Fragment>
+          <h2>{props.currentSelection.title}</h2>
+          <p>{props.currentSelection.body}</p>
+          <button onClick={props.handleEdit}>Edit</button>
+        </Fragment> )
+    };
 }
+  
+  
+
 
 export default NoteViewer;
